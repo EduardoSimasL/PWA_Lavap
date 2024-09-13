310c2e71c9b56abf1ae983f5101e7ca4a91f4674
Nicolas Santana <nicolassantanadev@gmail.com>
2024-09-13 19:57:43 -0300

    Ajuste em tela de login, e popup adicionado em tela de agendamentos no icone de lixo

--- js/agendamento.js
+++ js/agendamento.js
@@ -1,7 +1,21 @@
-function abrir(popupId) {
+/* function abrir(popupId) {
     document.getElementById(popupId).style.display = "block";    
 }
 
 function fechar(popupId) {
     document.getElementById(popupId).style.display = "none";    
 }
+*/
+
+function showPopup() {
+    document.getElementById('popup').style.display = 'flex';
+}
+
+document.getElementById('sim').addEventListener('click', function() {
+    alert('Agendamento cancelado');
+    document.getElementById('popup').style.display = 'none'; // Esconde o popup depois de cancelar
+});
+
+document.getElementById('nao').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'none'; // Esconde o popup
+});
